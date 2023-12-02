1ac648e3012f13dd9ea1a55e9bdffc0acc974cda
Hikmatillo019 <rayimjonovhikmatillo3@gmail.com>
2023-12-02 18:55:00 +0500

    fgh

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import google from '../assets/social-logos.png'
+export {google}
+import facebook from '../assets/Facebook.png'
+export {facebook}
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import './SignIn.css'
+import { facebook, google } from '../components'
 function SignIn() {
   return (
-    <div>
-
-        
-      
+    <div className='Sign_in'>
+        <div className="sign_epson">
+            <div className="sing_text">
+                <h1>Sign in</h1>
+                <div className="google_enter">
+                    <img src={google} alt="" />
+                    <h1>Continue with Google</h1>
+                </div>
+                <div className="facebook_enter">
+                    <img src={facebook} alt="" />
+                    <h1>Continue with Google</h1>
+                </div>
+            </div>
+            <div className="or">
+              <hr />
+              <span>OR</span>
+              <hr />
+            </div>
+            <div className="signin_login">
+                <div className="sign_inp">
+                  <p>Your username</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
