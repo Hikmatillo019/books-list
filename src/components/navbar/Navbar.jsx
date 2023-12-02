cb7ba992bd230c940b4f3d1c70299b13889df132
Hikmatillo019 <rayimjonovhikmatillo3@gmail.com>
2023-12-02 14:25:36 +0500

    full

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const  Navbar =() => {
+  return (
+    <div className='navbar'>
+        <h1>Salom World</h1>
+
+
+        
+      
+    </div>
+  )
+}
+
+export default Navbar
